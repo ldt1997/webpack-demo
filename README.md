b4afe52970766334dec6712a8eb04ee2366e5dc5
ldt1997 <474376875@qq.com>
2019-08-01 16:22:06 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,11 @@
+# node 环境搭建及 webpack 简单配置
+
+一个 webpack 练习 demo 1.打包`npm start` 2.运行本地服务器`npm run server`
+
+#Todo# 1.掌握 node 环境搭建，熟悉使用 npm 指令；
+
+2.掌握 webpack 的简单配置；
+
+3.独立搭建以上项目环境，使用 webpack 进行一些常规配置。
+
+**安装环境**：macOX
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import styles from "./App.less";
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.root}>
+        <div className={styles.content}>hi:D</div>
+      </div>
+    );
+  }
+}
+export default App;
